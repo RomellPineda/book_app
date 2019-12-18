3021d19be89f71fbd44708f3c5a3373598087907
Romell Pineda <romell.pineda@gmail.com>
2019-12-17 18:48:38 -0800

    operational server

--- server.js
+++ server.js
@@ -29,36 +29,33 @@ app.get('/hello', (req, res) => {
 
 // });
 
-app.post('/', (req, res) => {
-  superagent.get(`https://www.googleapis.com/books/v1/volumes?q=author+inauthor:${req.body.author}`).then(data => {
-    // console.log('items', data.body.items[0]);
+app.post('/searches', (req, res) => {
+  superagent.get(`https://www.googleapis.com/books/v1/volumes?q=${req.body.query}+in${req.body.search}`).then(data => {
 
-    // let image = data.body.items[0].volumeInfo.imageLinks.smallThumbnail;
-    // let title = data.body.items[0].volumeInfo.title;
-    // let author = data.body.items[0].volumeInfo.authors;
 
-    // let summary = data.body.items[0].volumeInfo.description;
-    // let isbn = data.body.items[0].volumeInfo.industryIdentifiers[0].identifier;
-    // let categorie = data.body.items[0].volumeInfo.categories;
-
-    // let testBook = new Book(image, title, author, summary, isbn, categorie);
+  // let testBook = new Book(image, title, author, summary, isbn, categorie);
     // console.log('test book', testBook);
-
-    const books = data.body.items[0].volumeInfo.map(book => console.log(book));
+    
+    const books = data.body.items.map(book => new Book(book));
+    
     // console.log(data.body.items[0].volumeInfo.authors);
     res.render('pages/searches', { books: books });
   });
-
+  
 });
 
-function Book(image, title, author, summary, isbn, categorie) {
-  this.image = image;
-  this.title = title;
-  this.author = author;
-  this.summary = summary;
-  this.isbn = isbn;
-  this.categorie = categorie;
+function Book(bookObj) {
+  this.image = bookObj.volumeInfo.imageLinks && bookObj.volumeInfo.imageLinks.thumbnail;
+  this.title = bookObj.volumeInfo.title;
+  this.author = bookObj.volumeInfo.authors;
+  
+  this.summary = bookObj.volumeInfo.description;
+  this.categorie = bookObj.volumeInfo.categories;
+  
+  //ISBN work when all other properties on the constructor are ommented out
+  // this.isbn = bookObj.volumeInfo.industryIdentifiers[0].identifier;
+};
 
-}
+
 
 app.listen(PORT, () => console.log(`app running on ${PORT}`));
